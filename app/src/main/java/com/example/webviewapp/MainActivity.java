ab6f998422f0d65b36f0212aea493d42c4f4a08a
b23ebbst <b23ebbst@his.student.se>
2024-04-08 16:31:59 +0200

    Ändrat namn på appen.
    Enabled internet-access.
    
    Ändrat TextView till WebView och angett ID för WebView.

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
